7772d9653f68d2c8bbdf0aeaba6ca657e0dd25c3
Chairat <chairat4567@gmail.com>
2019-03-05 11:46:03 +0700

    Update 12.00

---
+++ app/src/main/java/masterung/th/in/androidthai/bsruservice/MyConstant.java
@@ -0,0 +1,15 @@
+package masterung.th.in.androidthai.bsruservice;
+
+public class MyConstant {
+
+    private String urlAddUser = "https://www.androidthai.in.th/bsru/addUserMaster.php";
+    private String urlGetUserWhere = "http://www.androidthai.in.th/bsru/getUserWhereUserChairat.php";
+
+    public String getUrlGetUserWhere() {
+        return urlGetUserWhere;
+    }
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
